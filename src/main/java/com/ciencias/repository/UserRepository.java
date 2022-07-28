9c5a4c0335546676cd77c47dc3dad68dadefdcab
Brian Murillo <porrovespo54@gmail.com>
2022-07-27 19:33:45 -0500

    repository and service of user and roles completed . Close #4

---
+++ src/main/java/com/ciencias/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.ciencias.repository;
+
+import com.ciencias.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Set;
+
+@Repository
+public interface UserRepository extends JpaRepository<Usuario,Long> {
+    //Ejemplo de consulta
+    public Set<Usuario> findByUsername(String username);
+    //un suponer que quisieramos traer a todos los usuarios activos
+    //public Set<Usuario> findAllByStatus(String status);
+}
