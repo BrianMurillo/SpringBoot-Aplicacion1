f2099f68035b5117d562cd872718216b6db583dd
Brian Murillo <porrovespo54@gmail.com>
2022-09-14 12:56:56 -0500

    implement change password. Close #8

--- src/main/java/com/ciencias/service/UsuarioService.java
+++ src/main/java/com/ciencias/service/UsuarioService.java
@@ -1,5 +1,6 @@
 package com.ciencias.service;
 
+import com.ciencias.dto.ChangePasswordForm;
 import com.ciencias.entity.Usuario;
 
 public interface UsuarioService {
@@ -10,4 +11,6 @@ public interface UsuarioService {
 
     public Usuario updateUser(Usuario usuario) throws Exception;
     public void deleteUsuario(Long id)throws Exception;
+    public Usuario changePassword(ChangePasswordForm form) throws Exception;
+
 }
